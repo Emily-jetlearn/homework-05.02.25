af28d0c3af762774846803777e56e77a5c514fd2
Emily-jetlearn <mimidee176@gmail.com>
2025-02-05 18:11:21 +0000

    Add files via upload

---
+++ homework05.02.25.py
@@ -0,0 +1,18 @@
+from tkinter import
+
+win = Tk()
+win.geometry("700x700")
+win.title("calendar") 
+win.config(bg="#8fdbd9") 
+
+title=Label(win,text="CALENDAR",bg="#327da8",width=60,height=2, font='Helvetica', 12)
+title.stack
+
+label1=Label(win,text="Enter year",bg="#9fe3c4",width=2,height=1)
+
+b=button(win)
+b.stack
+
+
+
+
